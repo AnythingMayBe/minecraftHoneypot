eb3abd7d316384e3c9287d38db9fe0458cac61f3
AnythingMayBe <anythingmaybe@protonmail.com>
2022-11-14 19:30:45 +0100

    Added detection on ping request

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import json
 from quarry.net.server import ServerFactory, ServerProtocol
 from twisted.internet import reactor
 
-class Protocol(ServerProtocol): pass
+class Protocol(ServerProtocol):
+    def packet_status_request(self, buff):
+        print(f"Listening on: {self.connect_host}:{self.connect_port} Protocol: {self.protocol_mode} Address: {self.remote_addr}")
 
 class Factory(ServerFactory):
     protocol = Protocol
