d3143979892cffcbcae63c57d424a0bedddb50c4
AnythingMayBe <anythingmaybe@protonmail.com>
2022-11-15 19:35:02 +0100

    Added Discord Webhook integration

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import json
 from quarry.net.server import ServerFactory, ServerProtocol
 from twisted.internet import reactor
+import requests
 
 # Base
 def jsonloads():
     with open("config.json", "r") as file:
+        global config
         config = json.loads(file.read())
         file.close()
+
+# Webhook request
+def sendAlert(message):
+    if config["webhook"] != "": # If you want to disable the webhook alert, simply change the webhook parameter to ''
+        data = {"content": message}
+        requests.post(config["webhook"], data=data)
+    else: print(message)
 
 # Quarry
 class Protocol(ServerProtocol):
     def packet_status_request(self, buff):
-        print(f"PING | Listening on: {self.connect_host}:{self.connect_port} Protocol: {self.protocol_version} Address: {self.remote_addr}")
+        sendAlert(f"PING | Listening on: {self.connect_host}:{self.connect_port} Protocol: {self.protocol_version} Address: {self.remote_addr}")
 
     def packet_login_start(self, buff):
         buff.discard()
-        print(f"JOIN | Listening on: {self.connect_host}:{self.connect_port} Protocol: {self.protocol_version} Address: {self.remote_addr}")
+        sendAlert(f"JOIN | Listening on: {self.connect_host}:{self.connect_port} Protocol: {self.protocol_version} Address: {self.remote_addr}")
         self.close("bye")
 
 
